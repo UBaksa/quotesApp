c5bf0c82b7a6c3ca8daf90aca14e91bb7d6f5f3a
UBaksa <ujkanovicbakir03@gmail.com>
2023-11-01 09:54:50 +0100

    footer done

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import "./App.css";
 import LoginCard from "./components/LoginCard/LoginCard";
+import Footer from "./components/LoginCard/Footer/Footer";
 
 function App() {
   return (
     <div className="app">
       <LoginCard></LoginCard>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Login.css";
+import LoginCard from "../../components/LoginCard/LoginCard";
+
+export default function Login() {
+  return (
+    <div>
+      <LoginCard></LoginCard>
+    </div>
+  );
+}
